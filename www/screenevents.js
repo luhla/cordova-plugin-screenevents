e2f9c8bbdd51db2f179852a5f9563db13d57edbd
Lukáš Hladík <hladik@lh-soft.cz>
2023-03-31 15:47:59 +0200

    Update screenevents.js
    mistake definition

--- www/screenevents.js
+++ www/screenevents.js
@@ -1,8 +1,5 @@
-cordova.define("cordova-plugin-screenevents.ScreenEvents", function(require, exports, module) {
-    module.exports = {
-        listenerInit: function(successCallback, errorCallback,evt) {
-            cordova.exec(successCallback, errorCallback, 'ScreenEvents', 'listenerInit', [evt]);
-        }
-    };
-});
-    
+module.exports = {
+    listenerInit: function(successCallback, errorCallback,evt) {
+        cordova.exec(successCallback, errorCallback, 'ScreenEvents', 'listenerInit', [evt]);
+    }
+};
